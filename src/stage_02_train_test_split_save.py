b883edc1d36ec3de7aa53599e6aa22dd39174302
padmanabhan poraiyar <padmanabhanpaddy10@gmail.com>
2022-05-09 12:16:59 +0530

    Now Reads the train_test_split parameters from params.yaml

--- src/stage_02_train_test_split_save.py
+++ src/stage_02_train_test_split_save.py
@@ -7,9 +7,10 @@ import numpy as np
 import logging
 from sklearn.model_selection import train_test_split
 
-def save_train_test_split(config_path):
-    # Load the config file
+def save_train_test_split(config_path,params_path):
+    # Load the config file & params file
     config = read_yaml(config_path)
+    params = read_yaml(params_path)
 
     # Get the path of raw data dir to extract features
     clean_data_path = os.path.join("artifacts",config['local_data_dir'][0])
@@ -19,9 +20,8 @@ def save_train_test_split(config_path):
     Y = np.load(f"{clean_data_path}/Y.npy")
 
 
-    print("\n\nLoaded\n\n")
     # Perform Train-Test-Split 
-    print(X.shape)
+    train_size, random_state = params['train_test_split']['train_size'],params['train_test_split']['random_state']
     X_train, X_test, Y_train, Y_test = train_test_split(X,Y, train_size=0.8,random_state=12)
     logging.info("Train Test Split Successful")
 
@@ -31,7 +31,7 @@ def save_train_test_split(config_path):
     np.save(f"artifacts/{config['local_data_dir'][2]}/X_test.npy",X_test)
     np.save(f"artifacts/{config['local_data_dir'][2]}/Y_test.npy",Y_test)
     
-
+    # Log the activity
     logging.info("Saved Train Test Split Data Successfully")
     
 
@@ -41,6 +41,8 @@ if __name__ == "__main__":
 
     # Add a argument
     args.add_argument("--config",'-c',default='config/config.yaml')
+    args.add_argument("--params",'-p',default='params.yaml')
+
 
     # Call the parse_args method
     parsed_args = args.parse_args()
@@ -48,7 +50,7 @@ if __name__ == "__main__":
     try:
         logging.info("============== Stage 02: Save Train Test Split (Initiated) ==========================")
         # Get the data 
-        save_train_test_split(config_path=parsed_args.config)
+        save_train_test_split(config_path=parsed_args.config,params_path=parsed_args.params)
         # Log the status of the first Stage
         logging.info("============== Stage 02: Save Train Test Split (SUCCESSFUL) =========================\n\n")
     except Exception as e:
